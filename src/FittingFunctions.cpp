0fa4ab62d4c82e48cabf0f09b8b1f38451f04f59
mishashe <m.sheinman@nki.nl>
2021-06-11 00:04:53 +0200

    <commit_message>

--- src/FittingFunctions.cpp
+++ src/FittingFunctions.cpp
@@ -327,11 +327,11 @@ List Fit(arma::mat x0, arma::vec y, arma::vec tV, double lam1, double lam2,
     }
   }
   double LLprev = 0;
-  arma::vec betaPrev = beta;
+  arma::vec betaPrev = -(beta+0.001);
   while (abs(LL-LLprev)/sqrt(LLprev*LLprev+LL*LL)>1.0e-5 |
           any(sgn(beta) != sgn(betaPrev)))
   { 
-    Rcout<<LL<<std::endl;
+    Rcout<<" dfgdfgd "<<LL<<std::endl;
     LLprev = LL;
     betaPrev = beta;
     Rcout<<LL<<std::endl;
