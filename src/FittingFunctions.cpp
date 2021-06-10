0f97b49b72f076640248998ef9ef18f7dd821947
mishashe <m.sheinman@nki.nl>
2021-06-10 22:30:11 +0200

    try

--- src/FittingFunctions.cpp
+++ src/FittingFunctions.cpp
@@ -306,7 +306,8 @@ List FitRound(arma::mat x0, arma::vec y, arma::vec tV, double lam1, double lam2,
     }
   }
 
-  double LL=0;
+  double LL=0;   return(List::create(Named("beta") = beta, Named("Intercept") = Intercept, Named("LL") = LL));
+
   for (int s=0;s<ns;s++) {
     LL += -y(s)*log(p(s))*w(s) - (1-y(s))*log(1.0-p(s))*w(s);
   }
