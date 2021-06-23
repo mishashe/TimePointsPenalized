bc34e5b6d5a0db5e63dc74f034db3e2aa872784e
mishashe <m.sheinman@nki.nl>
2021-06-23 15:49:48 +0200

    added cv

--- src/FittingFunctions.cpp
+++ src/FittingFunctions.cpp
@@ -343,6 +343,7 @@ List Fit(arma::mat x0, arma::vec y, arma::vec tV, double lam1, double lam2,
     Rcout<<" LL 2 "<<LL<<" "<<LLprev<<" "<<fabs(LL-LLprev) <<" "<<sqrt(LLprev*LLprev+LL*LL)<<" "<<any(sgn(beta) != sgn(betaPrev))<<std::endl;
     Rcout<<(fabs(LL-LLprev)/sqrt(LLprev*LLprev+LL*LL)>1.0e-5 | any(sgn(beta) != sgn(betaPrev)))<<std::endl;
     dontstop = (fabs(LL-LLprev)/sqrt(LLprev*LLprev+LL*LL)>1.0e-5 | any(sgn(beta) != sgn(betaPrev)));
+    Rcout<<dontstop<<std::endl;
   }
   return(List::create(Named("beta") = beta, Named("Intercept") = Intercept, Named("nG") = accu(beta!=0)));
 }
