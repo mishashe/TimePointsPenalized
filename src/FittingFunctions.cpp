aa0e257a133443e14ee510473ea81dab5ff7e08d
mishashe <m.sheinman@nki.nl>
2021-06-23 20:59:23 +0200

    added cv

--- src/FittingFunctions.cpp
+++ src/FittingFunctions.cpp
@@ -329,13 +329,8 @@ List Fit(arma::mat x0, arma::vec y, arma::vec tV, double lam1, double lam2,
   do{
     LLprev = LL;
     betaPrev = arma::vec(beta);
-    //SingleGeneRound(x0, y, tV, lam1, lam2, beta, Intercept, w, IndFor0,IndTFor0, M, LL);
-    //GroupRound(x0, y, tV, lam1, lam2, beta, Intercept, w, IndFor0,IndTFor0, M, LL);
-    Rcout<<"LL = "<<LL<<" LLprev = "<<LLprev<<std::endl;
-    Rcout<<"sign change = "<<any(sgn(beta) != sgn(betaPrev))<<std::endl;
-    Rcout<<"keep = "<<((fabs(LL-LLprev)/sqrt(LLprev*LLprev+LL*LL)>1.0e-5) | (any(sgn(beta) != sgn(betaPrev))))<<std::endl;
-    count++;
-    Rcout<<"count = "<<count<<std::endl;
+    SingleGeneRound(x0, y, tV, lam1, lam2, beta, Intercept, w, IndFor0,IndTFor0, M, LL);
+    GroupRound(x0, y, tV, lam1, lam2, beta, Intercept, w, IndFor0,IndTFor0, M, LL);
   }
   while ((fabs(LL-LLprev)/sqrt(LLprev*LLprev+LL*LL)>1.0e-5) | (any(sgn(beta) != sgn(betaPrev))));
   return(List::create(Named("beta") = beta, Named("Intercept") = Intercept, Named("nG") = accu(beta!=0)));
